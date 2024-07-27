2d0047b64f3e71a47fe7fbbb8954b5a9033eb45f
danlinhqn <opinvn@gmail.com>
2024-07-27 13:23:47 +0700

    update

---
+++ render.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template_string("""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Web Page</title>
+        <style>
+            html, body {
+                height: 100%;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+            }
+            iframe {
+                width: 100%;
+                height: 100%;
+                border: none;
+            }
+        </style>
+    </head>
+    <body>
+        <iframe src="https://linkbio.co/trumriviu"></iframe>
+    </body>
+    </html>
+    """)
+
+if __name__ == '__main__':
+    app.run(debug=True)
